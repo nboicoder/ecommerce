812953022932bb89f8e6f0c5e50ee90f3a4924ec
Dexter <brianmauriceoluoch@gmail.com>
2026-01-13 12:40:31 +0300

    sanity added

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,13 @@
+import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <main>{children}</main>
+      <SanityLive />
     </ClerkProvider>
   );
 }
+
+export default Layout;
--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { sanityFetch } from "@/sanity/lib/live";
+import { ALL_CATEGORIES_QUERY } from "@/lib/sanity/queries/categories";
 
-export default function Home() {
+export default async function Home() {
+  const {} = await sanityFetch({
+    query: ALL_CATEGORIES_QUERY,
+  });
+
   return (
     <div>
       {/* Featured Products Carousel */}
-
+      <Button>Click me</Button>
       {/* Page Banner  */}
-
       {/* Products Section  */}
     </div>
   );
